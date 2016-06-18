dd891d73ea653090c6e53a924475c64e2eca581e
brmiller <brmiller@bdkk.net>
2016-06-17 20:35:34 -0700

    Reformatting, update keybindings, add run-on-startup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -168,12 +168,12 @@ mypromptbox = {}
 mylayoutbox = {}
 mytaglist = {}
 mytaglist.buttons = awful.util.table.join(
-                    awful.button({ }, 1, awful.tag.viewonly),
+                    awful.button({ },        1, awful.tag.viewonly),
                     awful.button({ modkey }, 1, awful.client.movetotag),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
+                    awful.button({ },        3, awful.tag.viewtoggle),
                     awful.button({ modkey }, 3, awful.client.toggletag),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
+                    awful.button({ },        4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+                    awful.button({ },        5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
                     )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
@@ -302,38 +302,65 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-    awful.key({ modkey, "Control" }, "r", awesome.restart),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey, "Control" }, "r",      awesome.restart),
+    awful.key({ modkey, "Shift"   }, "q",      awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+--    awful.key({ modkey,           }, "l",      function () awful.tag.incmwfact( 0.05)    end),
+--    awful.key({ modkey,           }, "h",      function () awful.tag.incmwfact(-0.05)    end),
+--    awful.key({ modkey, "Shift"   }, "h",      function () awful.tag.incnmaster( 1)      end),
+--    awful.key({ modkey, "Shift"   }, "l",      function () awful.tag.incnmaster(-1)      end),
+--    awful.key({ modkey, "Control" }, "h",      function () awful.tag.incncol( 1)         end),
+--    awful.key({ modkey, "Control" }, "l",      function () awful.tag.incncol(-1)         end),
+--    awful.key({ modkey,           }, "space",  function () awful.layout.inc(layouts,  1) end),
+--    awful.key({ modkey, "Shift"   }, "space",  function () awful.layout.inc(layouts, -1) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
+
+    -- Move clients
+    awful.key({ modkey            }, "Down",  function () awful.client.moveresize(  0,  20,   0,   0) end),
+    awful.key({ modkey            }, "Up",    function () awful.client.moveresize(  0, -20,   0,   0) end),
+    awful.key({ modkey            }, "Left",  function () awful.client.moveresize(-20,   0,   0,   0) end),
+    awful.key({ modkey            }, "Right", function () awful.client.moveresize( 20,   0,   0,   0) end),
+    awful.key({ modkey            }, "Next",  function () awful.client.moveresize( 20,  20, -40, -40) end),
+    awful.key({ modkey            }, "Prior", function () awful.client.moveresize(-20, -20,  40,  40) end),
+--    awful.key({ modkey, "Control" }, "h",     function () awful.client.moveresize( -1,   0,   0,   0) end),
+--    awful.key({ modkey, "Control" }, "j",     function () awful.client.moveresize(  0,   1,   0,   0) end),
+--    awful.key({ modkey, "Control" }, "k",     function () awful.client.moveresize(  0,  -1,   0,   0) end),
+--    awful.key({ modkey, "Control" }, "l",     function () awful.client.moveresize(  1,   0,   0,   0) end),
 
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
 
+    awful.key({ modkey }, "s",
+              function ()
+                  awful.prompt.run({ prompt = "ssh: " },
+                          mypromptbox[mouse.screen].widget,
+                          function (host)
+                              awful.util.spawn("sshvt " .. host)
+                          end,
+                          nil,
+                          awful.util.getdir("cache") .. "/history_ssh")
+              end),
+ 
     awful.key({ modkey }, "x",
               function ()
                   awful.prompt.run({ prompt = "Run Lua code: " },
-                  mypromptbox[mouse.screen].widget,
-                  awful.util.eval, nil,
-                  awful.util.getdir("cache") .. "/history_eval")
+                          mypromptbox[mouse.screen].widget,
+                          awful.util.eval,
+                          nil,
+                          awful.util.getdir("cache") .. "/history_eval")
               end),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end),
+
+    -- brightness
+    awful.key({ }, "XF86MonBrightnessDown", function () awful.util.spawn("xbacklight -dec 10") end),
+    awful.key({ }, "XF86MonBrightnessUp",   function () awful.util.spawn("xbacklight -inc 10") end),
 
     -- volume
     awful.key({ }, "XF86AudioRaiseVolume",  function () awful.util.spawn("amixer set Master 9%+") end),
     awful.key({ }, "XF86AudioLowerVolume",  function () awful.util.spawn("amixer set Master 9%-") end),
     awful.key({ }, "XF86AudioMute",         function () awful.util.spawn("amixer set Master toggle") end)
-
 )
 
 clientkeys = awful.util.table.join(
@@ -342,6 +369,8 @@ clientkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
+    awful.key({ modkey,           }, "p",      function (c) awful.client.movetoscreen(c, c.screen-1) end),
+    awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                               end),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
     awful.key({ modkey,           }, "n",
         function (c)
@@ -353,7 +382,22 @@ clientkeys = awful.util.table.join(
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
             c.maximized_vertical   = not c.maximized_vertical
+        end),
+    awful.key({ modkey,           }, "v",
+        function (c)
+            c.maximized_vertical   = not c.maximized_vertical
+        end),
+    awful.key({ modkey,           }, "h",
+        function (c)
+            c.maximized_horizontal = not c.maximized_horizontal
+        end),
+    awful.key({ modkey, "Control" }, "h",     function (c)
+                awful.client.moveresize(0, 0, -c.size_hints.width_inc, 0)
+        end),
+    awful.key({ modkey, "Control" }, "l",     function (c)
+                awful.client.moveresize(0, 0, c.size_hints.width_inc, 0)
         end)
+ 
 )
 
 -- Bind all key numbers to tags.
@@ -507,4 +551,19 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+-- }}}
+
+
+-- {{{ Startup
+function run_once(prg)
+    if not prg then
+        do return nil end
+    end
+    awful.util.spawn_with_shell("pgrep -u $USER -f -x '" .. prg .. "' || (exec " .. prg .. ")")
+end
+
+run_once("nm-applet")
+awful.util.spawn_with_shell("xrdb -load -override ~/.Xdefaults")
+awful.util.spawn_with_shell("if [ `lsusb | grep -c 'Logitech, Inc. MX Revolution/G7 Cordless Mouse'` -eq 1 ]; then xmodmap -e 'pointer = 1 8 3 4 5 6 7 2 9'; else xmodmap -e 'pointer = 1 2 3 4 5 6 7 8 9'; fi")
+
 -- }}}
