87b2b5d789e35addf3b2f038aa7894411e24cbb6
Brendan Miller <brendan.miller@forgerock.com>
2017-03-20 13:14:42 -0700

    Add cpu/memory widgets

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -23,6 +23,29 @@ require("debian.menu")
 -- Volume (https://awesome.naquadah.org/wiki/Volume_control_and_display)
 require("volume")
 
+-- Initialize cpu widget
+cpuwidget = awful.widget.graph()
+-- Graph properties
+cpuwidget:set_width(50)
+cpuwidget:set_background_color("#494B4F")
+cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
+                    {1, "#AECF96" }}})
+-- Register widget
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+
+-- Initialize memory widget
+memwidget = awful.widget.graph()
+-- Progressbar properties
+memwidget:set_width(50)
+--memwidget:set_height(10)
+--memwidget:set_vertical(true)
+memwidget:set_background_color("#393B3F")
+memwidget:set_border_color(nil)
+memwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#AECF96"}, {0.5, "#88A175"}, 
+                    {1, "#FF5656"}}})
+-- Register widget
+vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
+
 -- Battery widget
 vicious.register(wibox.widget.textbox(), vicious.widgets.bat, function (widget, args)
         icon = args[1]
@@ -243,6 +266,8 @@ for s = 1, screen.count() do
     if s == 1 then 
         right_layout:add(wibox.widget.systray())
         right_layout:add(volume_widget)
+        right_layout:add(cpuwidget)
+        right_layout:add(memwidget)
         right_layout:add(batwidget)
         right_layout:add(baticon)
         right_layout:add(batpct)
